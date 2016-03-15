03af3eb21d780a488fbf9564c2197e710cf65d01
Simon Johansson <sijo@MacBook-Pro-2.local>
2016-03-15 22:41:52 +0100

    Init commit

---
+++ app.js
@@ -0,0 +1,47 @@
+
+var path = require('path');
+var http = require('http');
+
+var express = require('express');
+var socket = require('socket.io');
+
+var port = process.env.PORT || '3000';
+var app = express();
+
+var server = http.createServer(app);
+
+app.set('port', port);
+
+// view engine setup
+app.set('views', path.join(__dirname, 'views'));
+app.set('view engine', 'ejs');
+
+app.use(express.static(path.join(__dirname, 'public')));
+
+app.get('/', function(req, res, next) {
+  res.render('index', {});
+});
+
+// catch 404 and forward to error handler
+app.use(function(req, res, next) {
+  var err = new Error('Not Found');
+  err.status = 404;
+  next(err);
+});
+
+app.use(function(err, req, res, next) {
+  res.status(err.status || 500);
+  res.render('error', {
+    message: err.message,
+    error: err
+  });
+});
+
+var io = socket(server);
+io.on('connection', function (socket) {
+  socket.on('input', function (data) {
+    io.emit('direction', data);
+  });
+});
+
+server.listen(port);
