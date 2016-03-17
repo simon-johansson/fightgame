6ee0a443ec76ab221b1d7b9d391004426d609517
Simon Johansson <mail@simon-johansson.com>
2016-03-17 21:40:26 +0100

    Refactor front-end code
    Break out Player, utils and socket to seperate files

---
+++ public/javascript/socket.js
@@ -0,0 +1,32 @@
+const socket = io.connect();
+
+socket.on('player joined', function (data) {
+    entities.push(new Player(data.id));
+});
+
+socket.on('player disconnected', function (data) {
+    for(var i = 0; i < entities.length; i++) {
+        if(entities[i].id == data.id) {
+            entities.splice(i, 1);
+            break;
+        }
+    }
+});
+
+socket.on('direction', function (data) {
+    if (data.type === 'keydown') {
+        entities.forEach(function (player) {
+            if(player.id === data.id) {
+                player.pressed[data.key] = true;
+            }
+        });
+    }
+
+    if (data.type === 'keyup') {
+        entities.forEach(function (player) {
+            if(player.id === data.id) {
+                player.pressed[data.key] = false;
+            }
+        });
+    }
+});
